72ef9fa5188ace552c2a8c3e85ac93e92fb8006d
ogm710811 <orestes.garcia.martinez@gmail.com>
2018-09-08 00:42:37 -0400

    improve code

--- src/app/pipes/number-unique-letters-in-string.pipe.ts
+++ src/app/pipes/number-unique-letters-in-string.pipe.ts
@@ -1,3 +1,4 @@
+import { Letter } from './../models/letter';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
@@ -7,14 +8,13 @@ export class NumberUniqueLettersInStringPipe implements PipeTransform {
   transform(value: string[], args?: any): any {
     const result = [];
     const str = value.join('');
-    const letters = new Object();
 
     for (let x = 0, length = str.length; x < length; x++) {
+      const letters = new Object();
       const l = str.charAt(x);
       letters[l] = isNaN(letters[l]) ? 1 : letters[l] + 1;
+      result.push(letters);
     }
-    result.push(letters);
-
     return result;
   }
 }
